84dc191ce22ead2bf5597e02fe44249f93355166
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-02-26 20:54:23 +0000

    fix(libmake): add an assertion that checks that the function does not panic

--- tests/test_ascii.rs
+++ tests/test_ascii.rs
@@ -6,6 +6,9 @@ mod tests {
     fn test_generate_ascii_art() {
         let text = "Hello, world!";
         let font_file = "./resources/standard.flf";
-        generate_ascii_art(text, font_file);
+        assert!(std::panic::catch_unwind(|| {
+            generate_ascii_art(text, font_file);
+        })
+        .is_ok());
     }
 }
