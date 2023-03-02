2ced3386b46962010043802cf753bbd5a661f7f4
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-03-02 09:23:27 +0000

    fix(libmake): updated tests

--- tests/test_generator.rs
+++ tests/test_generator.rs
@@ -1,12 +1,14 @@
 #[cfg(test)]
 mod tests {
 
-    use libmake::utils::{get_csv_field, get_json_field, get_yaml_field};
+    use libmake::utils::{
+        get_csv_field, get_json_field, get_yaml_field,
+    };
     use std::path::Path;
 
     #[test]
     fn test_generate_via_csv() {
-        let file_path = "./tests/data/mylibrary.csv";
+        let file_path = "./data/mylibrary.csv";
         let field_author_index = 0;
         let value = get_csv_field(Some(file_path), field_author_index);
         assert_eq!(value, Some(vec!["Me".to_string()]));
@@ -27,7 +29,7 @@ mod tests {
 
     #[test]
     fn test_generate_via_json() {
-        let file_path = "/tests/data/mylibrary.json";
+        let file_path = "./data/mylibrary.json";
         let field_name = "mylibrary";
         let value = if Path::new(file_path).exists() {
             get_json_field(Some(file_path), field_name)
@@ -39,7 +41,7 @@ mod tests {
 
     #[test]
     fn test_generate_via_yaml() {
-        let file_path = "/tests/data/mylibrary.yaml";
+        let file_path = "./data/mylibrary.yaml";
         let field_name = "mylibrary";
         let value = if Path::new(file_path).exists() {
             get_yaml_field(Some(file_path), field_name)
