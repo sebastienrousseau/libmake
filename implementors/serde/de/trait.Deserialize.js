be4c3502f74e1fcb56ccea64675983341a8e2e6a
sebastienrousseau <sebastienrousseau@users.noreply.github.com>
2023-05-01 08:58:25 +0000

    Deploy documentation at 3ad054b61392fea5c2c597beaee8b57b0da2876a 3ad054b61392fea5c2c597beaee8b57b0da2876a

--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libmake":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.159/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"libmake/generator/struct.FileGenerationParams.html\" title=\"struct libmake::generator::FileGenerationParams\">FileGenerationParams</a>"]]
+"libmake":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.160/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"libmake/generator/struct.FileGenerationParams.html\" title=\"struct libmake::generator::FileGenerationParams\">FileGenerationParams</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
