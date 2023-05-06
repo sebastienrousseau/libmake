5981ec6fc65e1a8752b16b57136f57e184baee49
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-05-06 21:07:56 +0100

    fix(libmake): updated benches

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -4,9 +4,9 @@ extern crate libmake;
 use criterion::{criterion_group, criterion_main, Criterion};
 use libmake::run;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("libmake", |b| b.iter(|| run()));
+fn libmake_benchmark(c: &mut Criterion) {
+    c.bench_function("libmake", |b| b.iter(run));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, libmake_benchmark);
 criterion_main!(benches);
