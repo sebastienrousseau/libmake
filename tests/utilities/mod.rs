a926ce18f2b8af419df7a5abe7d146eb58edaccd
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2024-05-16 00:18:54 +0100

    test(libmake): :white_check_mark: add new tests for `test_uuid.rs`

--- tests/utilities/mod.rs
+++ tests/utilities/mod.rs
@@ -1,3 +1,5 @@
 /// The `test_directory.rs` module contains tests for the directory module.
 pub mod test_directory;
 
+/// The `test_uuid.rs` module contains tests for the UUID module.
+pub mod test_uuid;
